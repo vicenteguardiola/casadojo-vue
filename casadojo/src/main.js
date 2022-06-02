2a8f511ba679cac689daa1ddd794d070be56aaa4
vicenteguardiola@gmail.com <B!zentic0b>
2022-06-02 07:45:58 +0200

    Se añade Vuetify 3 al proyecto

--- casadojo/src/main.js
+++ casadojo/src/main.js
@@ -1,7 +1,32 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+//import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(router).mount('#app')
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+loadFonts()
+
+
+const app = createApp(App);
+
+const vuetify = createVuetify({
+   components,
+   directives,
+ })
+
+app.use(router);
+
+
+  
+ app.use(vuetify);
+  
+app.mount('#app');
--- casadojo/vue.config.js
+++ casadojo/vue.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
 
 module.exports = {
   devServer: {
     port: 8081
+  },
+
+  pluginOptions: {
+    vuetify: {}
   }
 }
